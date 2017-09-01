20b338907883fb2b0383d440566c2a6cb74eb6b0
Can Yu <can.yu@hopebaytech.com>
2017-09-01 14:51:19 +0800

    support config file

--- gl.py
+++ gl.py
@@ -1,15 +1,32 @@
+import ConfigParser
 import gitlab
+import os
 
 class Gitlab:
 
-    def __init__(self, url, token):
-        self.url = url
-        self.token = token
+    def __init__(self, url=None, token=None):
+        self.cfg_PATH = './auth.cfg'
+
+        if url is None:
+            if not os.path.exists(self.cfg_PATH):
+                print 'Use create_config first!'
+            config = ConfigParser.RawConfigParser()
+            config.read(self.cfg_PATH)
+            self.url = config.get('Auth', 'URL')
+            self.token = config.get('Auth', 'Token')
+
         self.gl = self.connect(self.url, self.token)
 
     def connect(self, url, token):
         gl = gitlab.Gitlab(url, token)
         gl.auth()
+        print
+        print '========================'
+        print 'Use for Auth'
+        print 'URL: {}'.format(url)
+        print 'Token: {}'.format(token)
+        print '========================'
+        print
         return gl
 
     def get_project_manager(self):
@@ -17,6 +34,7 @@ class Gitlab:
         return self.gl.projects
 
     def get_group_manager(self):
+        self.gm = self.gl.groups
         return self.gl.groups
 
     def get_group_object(self, group_id):
@@ -33,6 +51,7 @@ class Gitlab:
 
     def print_detail(self, object_list):
         print 'id-name'
+        print '-----------------'
         for obj in object_list:
             print '{}-{}'.format(obj.id, obj.name)
 
